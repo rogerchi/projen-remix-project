d93c89e8ab3659ff48f7e92d5bb302fa2edebae7
Roger Chi <roger@rogerchi.com>
2022-12-25 10:12:10 -0500

    feat: initial commit

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,11 +1,23 @@
 import { cdk } from 'projen';
+import { NpmAccess } from 'projen/lib/javascript';
 const project = new cdk.JsiiProject({
-  author: 'user',
-  authorAddress: 'user@domain.com',
+  author: 'Roger Chi',
+  authorAddress: 'roger@rogerchi.com',
   defaultReleaseBranch: 'main',
-  name: 'projen-remix-project',
+  name: '@rogerchi/projen-remix-project',
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/user/projen-remix-project.git',
+  repositoryUrl: 'https://github.com/rogerchi/projen-remix-project.git',
+  description:
+    'Projen external project type for a Remix (https://remix.run) application',
+  stability: 'experimental',
+  autoApproveUpgrades: true,
+  autoApproveOptions: {
+    secret: 'PROJEN_GITHUB_TOKEN',
+  },
+  releaseToNpm: true,
+  npmAccess: NpmAccess.PUBLIC,
+  peerDeps: ['projen'],
+  sampleCode: false,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
